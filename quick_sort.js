788ada2f59214f391d86d771b65987de8500546c
DmitryZuiko <zuiko.z@mail.ru>
2024-06-11 21:51:34 +0300

    quick sort

---
+++ quick_sort.js
@@ -0,0 +1,16 @@
+const quickSort = (arr) => {
+  if (arr.length <= 1)
+    return arr;
+
+  let pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for(let i = 1; i < arr.length; i++) {
+    arr[i] < pivot ? 
+      left.push(arr[i]) :
+      right.push(arr[i]); 
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+}
